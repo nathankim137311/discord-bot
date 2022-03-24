9a9cac11363f9f0055c8a1fcdb084ddcb7f8b5b6
Nathan Kim <nathankim137311@gmail.com>
2022-03-23 19:48:24 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+client.login(token);
